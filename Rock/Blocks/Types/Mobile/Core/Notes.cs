5baf2cde7a77572a7df4c6c88f253f86669b155c
Daniel Hazelbaker <daniel@triumph.tech>
2020-12-14 15:26:49 -0700

    - Added placeholder mobile Notes block.

---
+++ Rock/Blocks/Types/Mobile/Core/Notes.cs
@@ -0,0 +1,96 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using Rock.Attribute;
+using Rock.Common.Mobile.Blocks.Content;
+using Rock.Web.UI;
+
+namespace Rock.Blocks.Types.Mobile.Core
+{
+    /// <summary>
+    /// Displays entity notes to the user and allows adding new notes.
+    /// </summary>
+    /// <seealso cref="Rock.Blocks.RockMobileBlockType" />
+
+    [DisplayName( "Notes" )]
+    [Category( "Mobile > Core" )]
+    [Description( "Displays entity notes to the user and allows adding new notes." )]
+    [IconCssClass( "fa fa-sticky-note" )]
+
+    #region Block Attributes
+
+    [EntityTypeField( "Context Entity Type",
+        Description = "The type of entity that will provide context for this block",
+        IsRequired = false,
+        Key = AttributeKeys.ContextEntityType,
+        Order = 0 )]
+
+    #endregion
+
+    [ContextAware]
+    public class Notes : RockMobileBlockType
+    {
+        /// <summary>
+        /// The block setting attribute keys for the MobileContent block.
+        /// </summary>
+        public static class AttributeKeys
+        {
+            /// <summary>
+            /// The context entity type key
+            /// </summary>
+            public const string ContextEntityType = "ContextEntityType";
+        }
+
+        #region IRockMobileBlockType Implementation
+
+        /// <summary>
+        /// Gets the required mobile application binary interface version required to render this block.
+        /// </summary>
+        /// <value>
+        /// The required mobile application binary interface version required to render this block.
+        /// </value>
+        public override int RequiredMobileAbiVersion => 2;
+
+        /// <summary>
+        /// Gets the class name of the mobile block to use during rendering on the device.
+        /// </summary>
+        /// <value>
+        /// The class name of the mobile block to use during rendering on the device
+        /// </value>
+        public override string MobileBlockType => "Rock.Mobile.Blocks.Core.Notes";
+
+        /// <summary>
+        /// Gets the property values that will be sent to the device in the application bundle.
+        /// </summary>
+        /// <returns>
+        /// A collection of string/object pairs.
+        /// </returns>
+        public override object GetMobileConfigurationValues()
+        {
+            return new { };
+        }
+
+        #endregion
+
+        #region Action Methods
+
+
+        #endregion
+    }
+}
