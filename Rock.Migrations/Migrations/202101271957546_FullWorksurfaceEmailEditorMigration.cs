9a80a515306a2526687f6e7f7ea55384273f4447
Garrett Johnson <nvisiondesign@gmail.com>
2021-01-27 13:40:54 -0700

    - Added additional block settings to migration.

--- Rock.Migrations/Migrations/202101271957546_FullWorksurfaceEmailEditorMigration.cs
+++ Rock.Migrations/Migrations/202101271957546_FullWorksurfaceEmailEditorMigration.cs
@@ -50,6 +50,12 @@ namespace Rock.Migrations
             RockMigrationHelper.AddBlockAttributeValue( "4709764E-5378-4B7D-AC85-A5D06BE86ECA", "C80209A8-D9E0-4877-A8E3-1F7DBF64D4C2", @"False" );
             // Add Block Login Status to Layout: Full Worksurface, Site: Rock RMS 
             RockMigrationHelper.AddBlock( true, null, "C2467799-BB45-4251-8EE6-F0BF27201535".AsGuid(), "C2D29296-6A87-47A9-A753-EE4E9159C4C4".AsGuid(), "04712F3D-9667-4901-A49D-4507573EF7AD".AsGuid(), "Login Status", "Login", @"", @"", 0, "0E3484CA-06EA-46CD-A55E-A1A480965DC9" );
+            // Add Block Attribute Value // Block: Login Status // BlockType: Login Status // Block Location: Layout=Full Worksurface, Site=Rock RMS // Attribute: My Profile Page // Attribute Value: 08dbd8a5-2c35-4146-b4a8-0f7652348b25 
+            RockMigrationHelper.AddBlockAttributeValue("0E3484CA-06EA-46CD-A55E-A1A480965DC9","6CFDDF63-0B21-48FC-90AE-362C0E73420B",@"08dbd8a5-2c35-4146-b4a8-0f7652348b25");
+            // Add Block Attribute Value // Block: Login Status // BlockType: Login Status // Block Location: Layout=Full Worksurface, Site=Rock RMS // Attribute: My Settings Page // Attribute Value: cf54e680-2e02-4f16-b54b-a2f2d29cd932 
+            RockMigrationHelper.AddBlockAttributeValue("0E3484CA-06EA-46CD-A55E-A1A480965DC9","FAF7DAAF-4927-44A8-BF4B-080FF556EBB0",@"cf54e680-2e02-4f16-b54b-a2f2d29cd932");
+            // Add Block Attribute Value // Block: Login Status // BlockType: Login Status // Block Location: Layout=Full Worksurface, Site=Rock RMS // Attribute: Logged In Page List // Attribute Value: My Dashboard^~/MyDashboard 
+            RockMigrationHelper.AddBlockAttributeValue("0E3484CA-06EA-46CD-A55E-A1A480965DC9","1B0E8904-196B-433E-B1CC-937AD3CA5BF2",@"My Dashboard^~/MyDashboard");
             // Add Block Smart Search to Layout: Full Worksurface, Site: Rock RMS 
             RockMigrationHelper.AddBlock( true, null, "C2467799-BB45-4251-8EE6-F0BF27201535".AsGuid(), "C2D29296-6A87-47A9-A753-EE4E9159C4C4".AsGuid(), "9D406BD5-88C1-45E5-AFEA-70F9CFB66C74".AsGuid(), "Smart Search", "Header", @"", @"", 0, "AD1A8AD8-3E94-45F3-A4B4-6BD2AE72A133" );
         }
